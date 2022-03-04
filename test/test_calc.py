627088e1d96bb947d11dbc699583e7852857c21c
kawadumax <kazmax7@gmail.com>
2022-03-04 10:32:32 +0900

    間違えテスト

--- test/test_calc.py
+++ test/test_calc.py
@@ -1,6 +1,10 @@
-from src.calc import mul,div
+from src.calc import *
+
 def test_mul():
     assert 6 == mul(3, 2)
 
 def test_div():
     assert 6 == div(12,2)
+
+def test_plus():
+    assert 4 == plus(1,1) # 間違えたテスト
